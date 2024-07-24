7c9fbfa6dd5d4728ae362749fda0bce42c846f03
PC-DAVID <david.lopez.enggmail.com>
2024-07-24 17:53:25 -0500

    Includes base files for data layer

---
+++ projects/data/src/lib/data.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-data',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      data works!
+    </p>
+  `,
+  styles: ``
+})
+export class DataComponent {
+
+}
