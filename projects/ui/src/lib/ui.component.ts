6c4f39727804ad0cbf826cb97307cf9674a23e96
PC-DAVID <david.lopez.enggmail.com>
2024-07-24 17:54:27 -0500

    Includes base files for ui layer

---
+++ projects/ui/src/lib/ui.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ui',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ui works!
+    </p>
+  `,
+  styles: ``
+})
+export class UiComponent {
+
+}
